cc439888aa2761ce39e956dfe4120269414276f0
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-01-13 16:10:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import CharactersList from './components/CharactersList.vue';
+export default {
+  components:{
+    AppHeader,
+    CharactersList
+  }
+}
+</script>
+
+<template lang="">
+  <div>
+    <AppHeader/>
+    <CharactersList/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use './styles/generals.scss' as *;
+</style>
